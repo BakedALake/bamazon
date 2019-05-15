7ed0a9ab564a90679210ad5cdce018d5712f7661
Greg Baker <semperfifcd@gmail.com>
2019-05-15 15:28:09 -0400

    Completed customer app and added video of walkthrough.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,67 +3,117 @@ var inquirer = require("inquirer");
 
 // Connection info
 var connection = mysql.createConnection({
-    host: "127.0.0.1",
+  host: "127.0.0.1",
 
-    // Your username
-    user: "root",
+  // Your username
+  user: "root",
 
-    // Your password
-    password: "password",
-    database: "bamazondb"
+  // Your password
+  password: "password",
+  database: "bamazondb"
 });
 
 
 // ================================= Main =================================
 connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-   
-    console.log("connected as id " + connection.threadId);
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
 
-    displayAll();
-    connection.end();
+  console.log("connected as id " + connection.threadId);
+
+  displayAll();
+
 });
 
 // ================================= Functions =================================
 
-function displayAll(){
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        // Log all results of the SELECT statement
-        console.log(res);
-      });
+function displayAll() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    // Log all results of the table statement
+    console.table(res, ["item_id", "product_name", "price"]);
+    buyProduct();
+  });
 }
 
-function buyProduct(productID, amount){
-    console.log("Updating all Rocky Road quantities...\n");
-    var query = connection.query(
-      "UPDATE products SET ? WHERE ?",
-      [
-        {
-            stock_quantity: 100
-        },
-        {
-            item_id: "Rocky Road"
-        }
-      ],
-      function(err, res) {
-        console.log(res.affectedRows + " products updated!\n");
-        // Call deleteProduct AFTER the UPDATE completes
-        deleteProduct();
+function buyProduct() {
+  var buyID, buyQTY;
+  inquirer
+    .prompt({
+      name: "buyItem",
+      type: "input",
+      message: "Enter the item ID for the product you'd like to buy."
+    })
+    .then(function (answer) {
+      // Store the id.
+      buyID = answer.buyItem;
+
+      inquirer
+        .prompt({
+          name: "buyAmount",
+          type: "input",
+          message: "Enter the quantity of the product you'd like to buy."
+        })
+        .then(function (answer) {
+          // Store the amount.
+          buyQTY = answer.buyAmount;
+
+          checkStock(buyID, buyQTY);
+        });
+    });
+}
+
+function checkStock(buyID, buyQTY) {
+  var stockQTY;
+  connection.query("SELECT * FROM products WHERE item_id = " + buyID, function (err, res) {
+    if (err) throw err;
+    stockQTY = res[0].stock_quantity;
+    if (buyQTY > stockQTY) {
+      console.log("Bamazon doesn't have enough of that product. Please try again.");
+      buyProduct();
+    }
+    else {
+      updateItem(buyID, buyQTY, stockQTY);
+    }
+  });
+}
+
+function updateItem(buyID, buyQTY, stockQTY) {
+  var newQTY = stockQTY - buyQTY;
+  connection.query("UPDATE products SET ? WHERE item_id = " + buyID,
+    [
+      {
+        stock_quantity: newQTY
       }
-    );
-  
-    // logs the actual query being run
-    console.log(query.sql);
+    ], function (err, res) {
+      displayPurchase(buyID, buyQTY);
+    });
 }
 
-function checkStock(requestQty){
+function displayPurchase(buyID, buyQTY) {
+  var itemCost, totalCost;
+  connection.query("SELECT * FROM products WHERE item_id = " + buyID, function (err, res) {
+    if (err) throw err;
+    itemCost = res[0].price;
+    totalCost = itemCost * buyQTY;
+    console.log("Your total purchase is for $" + totalCost);
 
-}
-
-function updateItem(productID, amount){
-    
+    inquirer
+      .prompt({
+        name: "continue",
+        type: "confirm",
+        message: "Would you like to make another purchase?"
+      })
+      .then(function (answer) {
+        if (answer.continue) {
+          displayAll();
+        }
+        else {
+          console.log("Thank you for shopping with us!");
+          connection.end();
+        }
+      });
+  });
 }
